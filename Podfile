f8e8357492b0b440869538956bdd7ba462cce774
Nimesh Gurung <nimesh.gurung@homeretailgroup.com>
2016-12-30 12:35:04 +0000

    Completed project

--- Podfile
+++ Podfile
@@ -8,5 +8,5 @@ target 'Pokedox' do
    pod 'RxSwift',    '~> 3.0'
    pod 'RxCocoa',    '~> 3.0'
    pod 'RxAlamofire'
-
+   pod 'SwiftyJSON'
 end
--- Pods/Target Support Files/Pods-Pokedox/Pods-Pokedox-frameworks.sh
+++ Pods/Target Support Files/Pods-Pokedox/Pods-Pokedox-frameworks.sh
@@ -88,10 +88,12 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/RxAlamofire/RxAlamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxAlamofire/RxAlamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
